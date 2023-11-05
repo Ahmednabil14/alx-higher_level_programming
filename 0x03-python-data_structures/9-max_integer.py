9d9305841302b2beff7179e6d22f47892295ec34
Ahmednabil14 <ahmednabil14499@gmail.com>
2023-11-05 02:23:52 +0200

    9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,9 +2,7 @@
 def max_integer(my_list=[]):
     x = 0
     if len(my_list) != 0:
-        for i in my_list:
-             if i > x:
-                x = i
-        return x
+        my_list.sort()
+        return my_list[-1]
     else:
         return None
