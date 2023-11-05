49a44d65f68b1ea403261f29550caa00c1cba02e
Ahmednabil14 <ahmednabil14499@gmail.com>
2023-11-06 01:04:47 +0200

    100

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -12,5 +12,5 @@ Py_ssize_t i;
 printf("[*] Size of the Python List = %lu\n", size);
 printf("[*] Allocated = %lu\n", ((PyListObject *)p)->allocated);
 for (i = 0; i < Py_SIZE(p); i++)
-printf("Element %d: %s\n", i, Py_TYPE(PyList_GetItem(p, i))->tp_name);
+printf("Element %ld: %s\n", i, Py_TYPE(PyList_GetItem(p, i))->tp_name);
 }
