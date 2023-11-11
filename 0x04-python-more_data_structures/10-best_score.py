2081a023ed7687f7f55c188dddc7e896b4e08cbb
Ahmednabil14 <ahmednabil14499@gmail.com>
2023-11-11 19:39:40 +0200

    10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    num = 0
+    keey = ""
+    if a_dictionary != None:
+        for key, val in a_dictionary.items():
+            if val > num:
+                num = val
+                keey = key
+        return keey
+    else:
+        return None
