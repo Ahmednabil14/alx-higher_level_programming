4d81af295d75fe590283246393e04f7d7aa8dc9f
Ahmednabil14 <ahmednabil14499@gmail.com>
2023-11-12 01:15:40 +0200

    100

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,8 +2,11 @@
 def weight_average(my_list=[]):
     total = 0
     sub = 0
-    for ls in my_list:
-        total += ls[0] * ls[1]
-        sub += ls[1]
-    avr = total / sub
-    return avr
+    if my_list:
+        for ls in my_list:
+            total += ls[0] * ls[1]
+            sub += ls[1]
+        avr = total / sub
+        return avr
+    else:
+        return 0
