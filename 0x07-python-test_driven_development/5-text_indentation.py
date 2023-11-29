cae84c722ab9647f4c65465e63b402e0fa0d18b4
Ahmednabil14 <ahmednabil14499@gmail.com>
2023-11-30 00:20:04 +0200

    5

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -17,7 +17,8 @@ def text_indentation(text):
         raise TypeError("text must be a string")
     syntaxx = ""
     for i in range(len(text)):
-        if text[i] == " " and (text[i-1] == ":" or text[i-1] == "?" or text[i-1] == "."):
+        if text[i] == " " and (text[i-1] == ":" or text[i-1] == "?"
+                               or text[i-1] == "."):
             continue
         if text[i] == "." or text[i] == "?" or text[i] == ":":
             syntaxx += text[i] + "\n" + "\n"
