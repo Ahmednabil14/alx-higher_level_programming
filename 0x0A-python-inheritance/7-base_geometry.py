d38c9dcc93b50d28b8b688117ba48312c98bb3ad
Ahmednabil14 <ahmednabil14499@gmail.com>
2023-12-04 23:30:59 +0200

    7

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+program for geometry
+"""
+
+
+class BaseGeometry:
+    """
+    BaseGeometry class
+    """
+    def area(self):
+        """
+        function that raising exception
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        function that validate value
+        arguments:
+            name: string value
+            value: must be intger
+        exceptions:
+            TypeError: <name> must be an integer if value not int
+            ValueError: <name> must be greater than 0 if value < 0
+        """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
