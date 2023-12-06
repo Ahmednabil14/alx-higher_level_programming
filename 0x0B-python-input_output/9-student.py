5b168e67d130d70f5c69fc4bc0a5a63480bc1262
Ahmednabil14 <ahmednabil14499@gmail.com>
2023-12-06 02:14:17 +0200

    9

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Student to JSON
+"""
+
+
+class Student:
+    """
+    class that define students
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        instantiation method
+        arguments:
+            first_name: first name of student
+            last_name: last name of student
+            age: age of student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        that retrieves a dictionary representation of a Student instance
+        """
+        dictionary = {}
+        for attr in dir(self):
+            if attr[0] == "_" and attr[1] == "_":
+                continue
+            x = getattr(self, attr)
+            if not callable(x):
+                dictionary.update({attr: x})
+        return dictionary
