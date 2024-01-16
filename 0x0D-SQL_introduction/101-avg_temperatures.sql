d8d8dc9936b1f525233d5480e846066145c1e2b0
Ahmednabil14 <ahmednabil14499@gmail.com>
2024-01-17 00:51:07 +0200

    101

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,2 +1,2 @@
 -- get avr tempreture
-SELECT city, AVG(value) AS avg_temp FROM temperature GROUP BY city ORDER BY avg_temp DESC;
+SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
