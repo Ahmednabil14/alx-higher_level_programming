bdfba6baf4d3b8cda3df84d1831c33957cdc78e6
Ahmednabil14 <ahmednabil14499@gmail.com>
2024-02-12 22:45:50 +0200

    task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -5,11 +5,11 @@ const y = process.argv[2];
 if (parseInt(y)) {
   let sq = '';
   for (let i = 0; i < y; i++) {
-    sq += 'x';
+    sq += 'X';
   }
   for (let i = 0; i < y; i++) {
-    console.log(sq);    
+    console.log(sq);
   }
 } else {
-    console.log('Missing size');
+  console.log('Missing size');
 }
