6cb87912b9d1ec9a662deec1524c5a5df239275e
Ahmednabil14 <ahmednabil14499@gmail.com>
2024-02-14 20:39:53 +0200

    task 102

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+const fs = require('fs');
+let data = '';
+const fileA = process.argv[2];
+const fileB = process.argv[3];
+const fileC = process.argv[4];
+fs.readFile(fileA, 'utf8', function (err, fileA) {
+  if (err) {
+    return;
+  }
+  data += fileA + '\n';
+  fs.readFile(fileB, 'utf8', function (err, fileB) {
+    if (err) {
+      return;
+    }
+    data += fileB + '\n';
+    fs.writeFile(fileC, data, function (err) {
+      if (err) {
+        return;
+      }
+    });
+  }
+  );
+}
+);
