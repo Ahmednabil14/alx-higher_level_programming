b3a07e66ae5b0e0ddf0d9c603609bca4026c6d10
Ahmednabil14 <ahmednabil14499@gmail.com>
2024-03-01 00:03:52 +0200

    task 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""program to get peak number"""
+
+
+def find_peak(list_of_integers):
+    if not list_of_integers:
+        return None
+
+    return find_peak_recursive(list_of_integers, 0, len(list_of_integers) - 1)
+
+def find_peak_recursive(lst, first, last):
+    if first == last:
+        return lst[first]
+
+    mid = (first + last) // 2
+
+    if lst[mid] < lst[mid + 1]:
+        return find_peak_recursive(lst, mid + 1, last)
+    else:
+        return find_peak_recursive(lst, first, mid)
