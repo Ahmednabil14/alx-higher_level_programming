95a12e7a579725147086edbd6dbafebcb7a5425e
Ahmednabil14 <ahmednabil14499@gmail.com>
2024-02-29 20:20:20 +0200

    task 1

---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+# Write a script to display the body of a 200 status code response.
+response=$(curl -s -I "$1" | grep "HTTP/1.1 200 OK")
+if [[ $response ]]; then
+curl -s "$1"
+fi
