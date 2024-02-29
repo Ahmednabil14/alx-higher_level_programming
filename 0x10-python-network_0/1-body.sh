f980a8005a3399b655296dd97b842d3e5b9c5a9a
Ahmednabil14 <ahmednabil14499@gmail.com>
2024-02-29 20:48:54 +0200

    task 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 # Write a script to display the body of a 200 status code response.
+# send request and get the response
+response=$(curl -s -w "%{http_code}" "$1" | tail -n 1)
+if [[ $response -eq 200 ]]; then
 curl -s "$1"
+fi
