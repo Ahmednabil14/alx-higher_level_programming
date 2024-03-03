fba563366d1080bb370c4495e73c7d1a983913d1
Ahmednabil14 <ahmednabil14499@gmail.com>
2024-03-04 00:36:10 +0200

    task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    data = {"q": argv[1]}
+    resp = requests.post(url="http://0.0.0.0:5000/search_user", data=data)
+    if resp.status_code == 200:
+        try:
+            json = resp.json()
+            if json:
+                print("[{}] {}".format(json["id"], json["name"]))
+            else:
+                print("No result")
+        except requests.exceptions.JSONDecodeError:
+            print("Not a valid JSON")
