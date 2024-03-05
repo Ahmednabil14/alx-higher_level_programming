89d5c031e0e6642f5ea7cc90069d1102f288f270
Ahmednabil14 <ahmednabil14499@gmail.com>
2024-03-06 00:32:57 +0200

    task 100

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -5,17 +5,11 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    i = 0
     resp = requests.get(
         url="https://api.github.com/repos/{}/{}/commits".format(
             argv[1], argv[2]))
     if resp.status_code == 200:
         json_data = resp.json()
-        while i < 10:
-            try:
-                print("{}: {}".format(
-                    json_data[i].get("sha"), json_data[i].get(
-                        "commit").get("author").get("name")))
-            except Exception:
-                break
-            i += 1
+        for commit in commits[:10]:
+            print(commit.get('sha'), end=': ')
+            print(commit.get('commit').get('author').get('name'))
