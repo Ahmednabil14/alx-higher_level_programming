03cf2df559f3418833a51c87145e76d4a8eca3c4
Ahmednabil14 <ahmednabil14499@gmail.com>
2024-03-26 21:13:46 +0200

    task 2

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const request = require('request')
+
+request.get(process.argv[2], (err, resp)=>
+{
+    if (err)
+    {
+        console.log(err)
+    }
+    console.log(`code: ${resp.statusCode}`)
+})
